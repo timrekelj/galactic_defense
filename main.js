3696ec60ca9680303be3735e5908a46c6d12f901
Tim Rekelj <tim.rekelj@proton.me>
2023-11-30 10:17:52 +0100

    succesfully imported gltf but there are still some bugs, added docker deployment

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
+//? START LIVE SERVER: python -m http.server 3000
+
 import { GLTFLoader } from './common/engine/loaders/GLTFLoader.js'
 import { LitRenderer } from './common/engine/renderers/LitRenderer.js'
+import { UnlitRenderer } from './common/engine/renderers/UnlitRenderer.js'
 import { ResizeSystem } from './common/engine/systems/ResizeSystem.js'
 import { UpdateSystem } from './common/engine/systems/UpdateSystem.js'
 import { TurntableController } from './common/engine/controllers/TurntableController.js'
-import { RotateAnimator } from './common/engine/animators/RotateAnimator.js'
 import { Light } from './light.js'
 
 import {
@@ -13,55 +15,47 @@ import {
     Model
 } from './common/engine/core.js'
 
-const canvas = document.querySelector('canvas')
+const canvas = document.querySelector('canvas');
 
-// Setup renderer
-const renderer = new LitRenderer(canvas)
-await renderer.initialize()
+const renderer = new UnlitRenderer(canvas);
+await renderer.initialize();
 
-// GLTF loader
-const gltfloader = new GLTFLoader()
-await gltfloader.load('./common/models/rocks/rocks.gltf')
+const loader = new GLTFLoader();
+await loader.load('./assets/environment/floating_island.gltf');
 
-// Setup camera
-const scene = gltfloader.loadScene(gltfloader.defaultScene)
-const camera = scene.find(node => node.getComponentOfType(Camera))
+const scene = loader.loadScene(loader.defaultScene);
+if (!scene) { throw new Error('A default scene is required'); }
+
+const camera = scene.find(node => node.getComponentOfType(Camera));
+if (!camera) { throw new Error('A camera is required'); }
+
 camera.addComponent(new TurntableController(camera, document.body, {
     distance: 20
-}))
+}));
 
-// Setup model
-const model = gltfloader.loadNode('Geometry')
-// model.addComponent(new RotateAnimator(model, {
-//     startRotation: [0, 0, 0, 1],
-//     endRotation: [0, 0, 1, 0],
-//     duration: 5,
-//     loop: true
-// }))
+const light = new Node();
+light.addComponent(new Transform({ translation: [0, 2, 0] }));
+light.addComponent(new Light());
+scene.addChild(light);
 
-// Create the light
-const light = new Node()
-light.addComponent(new Transform({
-    translation: [0, 2, 0]
-}))
-light.addComponent(new Light())
-scene.addChild(light)
 
 function update(t, dt) {
     scene.traverse(node => {
         for (const component of node.components) {
-            component.update?.()
+            component.update?.();
         }
     })
 }
 
 function render() {
-    renderer.render(scene, camera)
+    renderer.render(scene, camera);
 }
 
 function resize({ displaySize: { width, height } }) {
-    camera.getComponentOfType(Camera).aspect = width / height
+    camera.getComponentOfType(Camera).aspect = width / height;
 }
 
-new UpdateSystem({ update, render }).start()
-new ResizeSystem({ canvas, resize }).start()
+new UpdateSystem({ update, render }).start();
+new ResizeSystem({ canvas, resize }).start();
+
+document.querySelector('.loader-container').remove();
